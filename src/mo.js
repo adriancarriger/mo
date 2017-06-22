b9264618d18b4b74b7c1b074daa6edcdfbd23e0d
adriancarriger <acarriger@docutap.com>
2017-06-22 08:00:52 -0500

    add warning if not a mo project

--- src/mo.js
+++ src/mo.js
@@ -4,6 +4,8 @@ const chalk = require('chalk');
 const clear = require('clear');
 const program = require('commander');
 const figlet = require('figlet');
+const fs = require('fs-extra');
+let showingHelp = false;
 
 program
   .version('0.0.1')
@@ -13,13 +15,29 @@ program
   .command('export', 'gets repo from mo-src, applies mo plugins, and puts it inside mo-dist');
 
 if (!process.argv.slice(2).length) {
+  showHelp();
+}
+
+fs.pathExists(`${process.cwd()}/mo.yml`).then(exists => {
+  if (!exists && !showingHelp) {
+    showHelp('It doesn\'t look like this is a Mo project. You may need to run `mo init`');
+  } else {
+    program.parse(process.argv);
+  }
+});
+
+function showHelp(message) {
+  showingHelp = true;
   clear();
   console.log(
     chalk.green(
       figlet.textSync('Mo', { horizontalLayout: 'full' })
     )
   );
+  if (message) {
+    console.log(
+      chalk.red(message)
+    );
+  }
   program.help();
 }
-
-program.parse(process.argv);
